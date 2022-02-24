e8a02ac59eddc2b31456762179addc329404c67d
linyaye <840567289@qq.com>
2022-02-24 11:33:13 +0800

    返回没有数据时，不进行处理

--- android/src/main/java/com/cpming/rn/unionpay/RCTUnionpayModule.java
+++ android/src/main/java/com/cpming/rn/unionpay/RCTUnionpayModule.java
@@ -44,48 +44,51 @@ public class RCTUnionpayModule extends ReactContextBaseJavaModule implements Act
         if (data == null) {
             return;
         }
-        WritableMap response = Arguments.createMap();
-        String msg = "";
-        /*
-         * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
-         */
-        String str = data.getExtras().getString("pay_result");
-        if (str.equalsIgnoreCase("success")) {
-            // 如果想对结果数据验签，可使用下面这段代码，但建议不验签，直接去商户后台查询交易结果
-            // result_data结构见c）result_data参数说明
-            if (data.hasExtra("result_data")) {
-                String result = data.getExtras().getString("result_data");
-                try {
-                    JSONObject resultJson = new JSONObject(result);
-                    String sign = resultJson.getString("sign");
-                    String dataOrg = resultJson.getString("data");
-                    response.putString("sign", sign);
-                    response.putString("data", dataOrg);
+
+        if(data.getExtras() != null){
+            WritableMap response = Arguments.createMap();
+                    String msg = "";
                     /*
-                    // 此处的verify建议送去商户后台做验签
-                    // 如要放在手机端验，则代码必须支持更新证书
-                    boolean ret = verify(dataOrg, sign, mMode);
-                    if (ret) {
-                        // 验签成功，显示支付结果
+                     * 支付控件返回字符串:success、fail、cancel 分别代表支付成功，支付失败，支付取消
+                     */
+                    String str = data.getExtras().getString("pay_result");
+                    if (str.equalsIgnoreCase("success")) {
+                        // 如果想对结果数据验签，可使用下面这段代码，但建议不验签，直接去商户后台查询交易结果
+                        // result_data结构见c）result_data参数说明
+                        if (data.hasExtra("result_data")) {
+                            String result = data.getExtras().getString("result_data");
+                            try {
+                                JSONObject resultJson = new JSONObject(result);
+                                String sign = resultJson.getString("sign");
+                                String dataOrg = resultJson.getString("data");
+                                response.putString("sign", sign);
+                                response.putString("data", dataOrg);
+                                /*
+                                // 此处的verify建议送去商户后台做验签
+                                // 如要放在手机端验，则代码必须支持更新证书
+                                boolean ret = verify(dataOrg, sign, mMode);
+                                if (ret) {
+                                    // 验签成功，显示支付结果
+                                    msg = "支付成功！";
+                                } else {
+                                    // 验签失败
+                                    msg = "支付失败！";
+                                }
+                                */
+                            } catch (JSONException e) {
+                            }
+                        }
+                        // 结果result_data为成功时，去商户后台查询一下再展示成功
                         msg = "支付成功！";
-                    } else {
-                        // 验签失败
+                    } else if (str.equalsIgnoreCase("fail")) {
                         msg = "支付失败！";
+                    } else if (str.equalsIgnoreCase("cancel")) {
+                        msg = "用户取消了支付";
                     }
-                    */
-                } catch (JSONException e) {
-                }
-            }
-            // 结果result_data为成功时，去商户后台查询一下再展示成功
-            msg = "支付成功！";
-        } else if (str.equalsIgnoreCase("fail")) {
-            msg = "支付失败！";
-        } else if (str.equalsIgnoreCase("cancel")) {
-            msg = "用户取消了支付";
+                    response.putString("pay_result", str);
+                    response.putString("msg", msg);
+                    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("UnionPayResponse", response);
         }
-        response.putString("pay_result", str);
-        response.putString("msg", msg);
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("UnionPayResponse", response);
     }
 
     @Override
